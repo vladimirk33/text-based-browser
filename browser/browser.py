9e2c24e766bc518768c8762babe3257e2b7a2918
Vladimir <mr.katenin@mail.ru>
2020-08-23 17:26:54 +0300

    added http requests

--- browser/browser.py
+++ browser/browser.py
@@ -1,51 +1,12 @@
 import sys
 import os
+import requests
 
 stack = []
 
-nytimes_com = '''
-This New Liquid Is Magnetic, and Mesmerizing
-
-Scientists have created “soft” magnets that can flow 
-and change shape, and that could be a boon to medicine 
-and robotics. (Source: New York Times)
-
-
-Most Wikipedia Profiles Are of Men. This Scientist Is Changing That.
-
-Jessica Wade has added nearly 700 Wikipedia biographies for
- important female and minority scientists in less than two 
- years.
-
-'''
-
-bloomberg_com = '''
-The Space Race: From Apollo 11 to Elon Musk
-
-It's 50 years since the world was gripped by historic images
- of Apollo 11, and Neil Armstrong -- the first man to walk 
- on the moon. It was the height of the Cold War, and the charts
- were filled with David Bowie's Space Oddity, and Creedence's 
- Bad Moon Rising. The world is a very different place than 
- it was 5 decades ago. But how has the space race changed since
- the summer of '69? (Source: Bloomberg)
-
-
-Twitter CEO Jack Dorsey Gives Talk at Apple Headquarters
-
-Twitter and Square Chief Executive Officer Jack Dorsey 
- addressed Apple Inc. employees at the iPhone maker’s headquarters
- Tuesday, a signal of the strong ties between the Silicon Valley giants.
-'''
-
-
-def is_valid_url(url):
-    if "." in url:
-        return True
-    return False
-
 
 def create_dir():
+    """Create a directory for pages."""
     try:
         dir_name = sys.argv[1]
     except IndexError:
@@ -57,11 +18,34 @@ def create_dir():
     return dir_name
 
 
+def full_url(url):
+    if url.startswith("https://"):
+        return url
+    else:
+        return "https://" + url
+
+
+def get_status(url):
+    try:
+        requests.get(url)
+    except:
+        return False
+    return True
+
+
+def get_cache(url):
+    response = requests.get(url)
+    return response.text
+
+
 def cache_url(dir_name: str, url: str, cache: str):
     full_path = dir_name + "/" + url[:url.rindex(".")] + ".txt"
-    with open(full_path, "w") as f:
+    with open(full_path, "w", encoding="utf-8") as f:
         f.write(cache)
+    with open(full_path, "r") as f:
+        cache = f.read()
     stack.append(cache)
+    return cache
 
 
 def read_cached_url(dir_name: str, url: str):
@@ -84,25 +68,20 @@ def main():
     user_input = ""
     while user_input != "exit":
         user_input = input("> ")
-        if is_valid_url(user_input):
-            if user_input == "nytimes.com":
-                cache_url(dir_name, user_input, nytimes_com)
-                print(nytimes_com)
-            elif user_input == "bloomberg.com":
-                cache_url(dir_name, user_input, bloomberg_com)
-                print(bloomberg_com)
-            else:
-                print("Error: Incorrect URL")
+        if user_input == "exit":
+            continue
+        elif user_input == "back":
+            back()
+            continue
+        url = full_url(user_input)
+        if get_status(url):
+            cache = get_cache(url)
+            print(cache_url(dir_name, user_input, cache))
         else:
-            if user_input == "exit":
-                pass
-            elif user_input == "back":
-                back()
-            else:
-                try:
-                    print(read_cached_url(dir_name, user_input))
-                except FileNotFoundError:
-                    print("Error: Incorrect URL")
+            try:
+                print(read_cached_url(dir_name, user_input))
+            except FileNotFoundError:
+                print("ConnectionError")
 
 
 if __name__ == "__main__":
