61d8378619b36411dd0ea58a4f56a82c30224020
Vladimir <mr.katenin@mail.ru>
2020-08-25 22:40:48 +0300

    added readable text

--- browser/browser.py
+++ browser/browser.py
@@ -1,6 +1,8 @@
 import sys
 import os
 import requests
+from colorama import init, Fore
+from bs4 import BeautifulSoup
 
 stack = []
 
@@ -19,6 +21,7 @@ def create_dir():
 
 
 def full_url(url):
+    """Return full url."""
     if url.startswith("https://"):
         return url
     else:
@@ -26,6 +29,7 @@ def full_url(url):
 
 
 def get_status(url):
+    """Return status (True/False)."""
     try:
         requests.get(url)
     except:
@@ -34,14 +38,21 @@ def get_status(url):
 
 
 def get_cache(url):
+    """Return text."""
     response = requests.get(url)
-    return response.text
+    soup = BeautifulSoup(response.content, 'html.parser')
+    page_text = [tags.get_text() for tags in soup.find_all('a')]
+    return page_text
 
 
 def cache_url(dir_name: str, url: str, cache: str):
+    """Return cache, make a cache file."""
     full_path = dir_name + "/" + url[:url.rindex(".")] + ".txt"
     with open(full_path, "w", encoding="utf-8") as f:
-        f.write(cache)
+        text = ''
+        for string in cache:
+            text = text + Fore.BLUE + string + "\n"
+        f.write(text)
     with open(full_path, "r") as f:
         cache = f.read()
     stack.append(cache)
@@ -49,6 +60,7 @@ def cache_url(dir_name: str, url: str, cache: str):
 
 
 def read_cached_url(dir_name: str, url: str):
+    """Read cached file."""
     full_path = dir_name + "/" + url + ".txt"
     with open(full_path, "r") as f:
         cache = f.read()
@@ -57,6 +69,7 @@ def read_cached_url(dir_name: str, url: str):
 
 
 def back():
+    """Return back."""
     if len(stack) > 1:
         print(stack.pop(-2))
     else:
@@ -64,6 +77,8 @@ def back():
 
 
 def main():
+    """Main function."""
+    init()
     dir_name = create_dir()
     user_input = ""
     while user_input != "exit":
@@ -75,7 +90,7 @@ def main():
             continue
         url = full_url(user_input)
         if get_status(url):
-            cache = get_cache(url)
+            cache: list = get_cache(url)
             print(cache_url(dir_name, user_input, cache))
         else:
             try:
